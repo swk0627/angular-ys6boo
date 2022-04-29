f6ab04522eb3ef60cdefb118e318bfcec4f3a436
swk0627 <kosawa0627@gmail.com>
2022-04-30 02:24:14 +0900

    Create cart view

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  items = this.cartService.getItems();
+
+  constructor(
+    private cartService: CartService
+  ) { }
 
   ngOnInit(): void {
   }
